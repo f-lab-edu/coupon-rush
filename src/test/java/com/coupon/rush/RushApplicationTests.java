b39bcad3ee99707bd8bc9904c579b33a799b48ad
jungwanhong-metanet <jungwan.hong.metanet@joyent.com>
2025-02-17 19:31:10 +0900

    chore: project create

---
+++ src/test/java/com/coupon/rush/RushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coupon.rush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
